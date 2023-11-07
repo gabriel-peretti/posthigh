92b891dc1d24d72c268ec133c3126dfe024f02ef
gabriel-peretti <gabriel.ferreira1@universo.univates.br>
2023-11-07 00:04:20 -0300

    fix

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,39 +1,43 @@
 'use client';
-import { InstagramLogin } from '@amraneze/react-instagram-login';
-import axios from 'axios';
+import { NextApiRequest } from 'next';
+import Link from 'next/link';
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-export default function Manage() {
-	const [user, setUser] = React.useState<any>(null);
+interface SearchProps {
+    searchParams: NextApiRequest['query'];
+}
 
-	async function getInstafeed() {
-		const token =
-			'IGQWRNMHE1N00wRkJhRFM3ZADF1OEpXaWVNbEVESE9VMWMyNldTWjFJaUMyZA2psTFR3dk9HallBUEFkWnNJS2tFWVphSmdJUWswWlkyTnQ5cFhyaTd2VUo0eDJVV2laQnNRamx5dk12WWhZAdFFKM3BFUTZAEck5xYUEZD';
-		const url = `https://graph.instagram.com/me?fields=id,username&access_token=${token}`;
-		const { data } = await axios.get(url);
-		setUser(data.username);
-		console.log(data.username);
-	}
+export default function Manage({ searchParams }: SearchProps) {
+    const search = searchParams;
+    console.log(search);
+    const [user, setUser] = React.useState<any>(null);
 
-	React.useEffect(() => {
-		getInstafeed();
-	}, []);
+    // async function getInstafeed() {
+    //     const token =
+    //         'IGQWRNMHE1N00wRkJhRFM3ZADF1OEpXaWVNbEVESE9VMWMyNldTWjFJaUMyZA2psTFR3dk9HallBUEFkWnNJS2tFWVphSmdJUWswWlkyTnQ5cFhyaTd2VUo0eDJVV2laQnNRamx5dk12WWhZAdFFKM3BFUTZAEck5xYUEZD';
+    //     const url = `https://graph.instagram.com/me?fields=id,username&access_token=${token}`;
+    //     const { data } = await axios.get(url);
+    //     setUser(data.username);
+    //     console.log(data.username);
+    // }
 
-	const responseInstagram = (response: any) => {
-		console.log(response);
-	};
+    // React.useEffect(() => {
+    //     getInstafeed();
+    // }, []);
 
-	return (
-		<>
-			<div>Usuario logado: {user}</div>
-			<InstagramLogin
-				clientId="347678657643128"
-				buttonText="Login with instagram"
-				onSuccess={responseInstagram}
-				onFailure={responseInstagram}
-				redirectUri="https://www.posthigh.com.br/"
-			/>
-		</>
-	);
+    return (
+        <>
+            <Link
+                href={`https://api.instagram.com/oauth/authorize
+        ?client_id=347678657643128
+        &redirect_uri=https://posthigh.com.br/manage
+        &scope=user_profile,user_media
+        &response_type=code`}
+            >
+                <div className="border border-red-500 w-20">login instagram</div>
+            </Link>
+
+            <div>Usuario logado: {user}</div>
+        </>
+    );
 }
