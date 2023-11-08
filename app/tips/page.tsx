37ed6b38209dee72d0224461f009a0ddaa452032
gabriel-peretti <gabriel.ferreira1@universo.univates.br>
2023-11-08 02:46:46 -0300

    alteracoes nas paginas

--- app/tips/page.tsx
+++ app/tips/page.tsx
@@ -10,8 +10,8 @@ export default function Tips() {
 		<>
 			<Header />
 			<div className="h-screen flex flex-col items-center justify-center bg-primary">
-				<span className="text-white">Dicas</span>
-				<span className="text-white">xerecrazy</span>
+				<span className="text-white">Dicas para publicações eficientes</span>
+				<span className="text-white">Otimize suas postagens nas redes sociais com dicas práticas. Aprenda a criar conteúdo envolvente, escolher as melhores imagens e escrever legendas cativantes.</span>
 			</div>
 			<svg
 				className="absolute -bottom-24 w-full"
@@ -26,48 +26,55 @@ export default function Tips() {
 				></path>
 			</svg>
 			<Section
-				title="Estratégias de crescimento nas redes sociais"
-				description="Descubra estratégias comprovadas para aumentar o alcance e o envolvimento nas redes sociais. Aprenda a conquistar seguidores, construir autoridade e alcançar resultados notáveis."
+				title="Conteúdo visual cativante"
+				description="Use imagens e vídeos de alta qualidade para atrair a atenção do seu público. Uma imagem vale mais que mil palavras, e um vídeo pode contar uma história envolvente."
 				image={section2}
 				buttonLabel="Acessar"
 				hasButton={false}
 			/>
 			<Section
-				title="Dicas para publicações eficientes"
-				description="Otimize suas postagens nas redes sociais com dicas práticas. Aprenda a criar conteúdo envolvente, escolher as melhores imagens e escrever legendas cativantes."
+				title="Legendas criativas"
+				description="Crie legendas que complementem sua imagem ou vídeo. Seja conciso e conte uma história relevante para envolver os seguidores."
 				image={section1}
 				buttonLabel="Acessar"
 				inverted
 				hasButton={false}
 			/>
 			<Section
-				title="Dicas para publicações eficientes"
-				description="Otimize suas postagens nas redes sociais com dicas práticas. Aprenda a criar conteúdo envolvente, escolher as melhores imagens e escrever legendas cativantes."
+				title="Hashtags estratégicas"
+				description="Pesquise e use hashtags relacionadas ao seu conteúdo para alcançar um público mais amplo. Mas evite o exagero; algumas hashtags relevantes são mais eficazes do que muitas irrelevantes."
 				image={section1}
 				buttonLabel="Acessar"
 				hasButton={false}
 			/>
 			<Section
-				title="Dicas para publicações eficientes"
-				description="Otimize suas postagens nas redes sociais com dicas práticas. Aprenda a criar conteúdo envolvente, escolher as melhores imagens e escrever legendas cativantes."
+				title="Postagens programadas"
+				description="Agende suas postagens para os horários em que seu público está mais ativo. Use a PostHigh para automatizar o processo."
 				image={section1}
 				buttonLabel="Acessar"
 				inverted
 				hasButton={false}
 			/>
 			<Section
-				title="Dicas para publicações eficientes"
-				description="Otimize suas postagens nas redes sociais com dicas práticas. Aprenda a criar conteúdo envolvente, escolher as melhores imagens e escrever legendas cativantes."
+				title="Interaja com seguidores"
+				description="Responda a comentários, mensagens e interaja com seus seguidores. Construir relacionamentos é fundamental."
 				image={section1}
 				buttonLabel="Acessar"
 				hasButton={false}
 			/>
 			<Section
-				title="Dicas para publicações eficientes"
-				description="Otimize suas postagens nas redes sociais com dicas práticas. Aprenda a criar conteúdo envolvente, escolher as melhores imagens e escrever legendas cativantes."
+				title="Explore recursos de histórias"
+				description="Use histórias do Instagram e Facebook para compartilhar atualizações diárias, bastidores e conteúdo efêmero que mantenha seu público engajado."
 				image={section1}
 				buttonLabel="Acessar"
 				inverted
+				hasButton={false}
+			/>
+			<Section
+				title="Acompanhe tendências"
+				description="Esteja atento às tendências e eventos atuais. Participar de conversas populares pode aumentar sua visibilidade."
+				image={section1}
+				buttonLabel="Acessar"
 				hasButton={false}
 			/>
 			<AdvertisementBanner />
