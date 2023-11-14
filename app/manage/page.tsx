e23d16caaef4a36aa190a7868c139d8487675afa
gabriel-peretti <gabriel.ferreira1@universo.univates.br>
2023-11-14 01:10:57 -0300

    implementando post twitter

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -2,62 +2,54 @@
 import { InstagramLogin } from '@amraneze/react-instagram-login';
 import axios from 'axios';
 import Link from 'next/link';
-import React from 'react';
 import { signIn } from 'next-auth/react';
 
-export default function Manage() {
-	const [user, setUser] = React.useState<any>(null);
+import React, { useState } from 'react';
 
-	async function getInstafeed() {
-		const token =
-			'IGQWRNS21SZAHhUYVpJaWVUbFVNcThWeVoyNUxoRDk5cUdCTlZAGR0hmNG05S2FvVkFDbTBKRjg4SzAyNU1KQ0hrRUNhZA0tRdl8ycW01RUc2R1JHWC1VdDBTQjBPNFpTeUhkTGViMEdXaFRHSW9OTXZAjQ2RxZAnluNVUZD';
-		const url = `https://graph.instagram.com/me?fields=id,username&access_token=${token}`;
-		const { data } = await axios.get(url);
-		setUser(data.username);
-		console.log(data);
-	}
+const TwitterPost = () => {
+  const [tweetText, setTweetText] = useState('');
 
-	const [caption, setCaption] = React.useState('Teste');
+  const handleTweetChange = (e) => {
+    setTweetText(e.target.value);
+  };
 
-	const handlePost = async () => {
-		const accessToken =
-			'IGQWRNS21SZAHhUYVpJaWVUbFVNcThWeVoyNUxoRDk5cUdCTlZAGR0hmNG05S2FvVkFDbTBKRjg4SzAyNU1KQ0hrRUNhZA0tRdl8ycW01RUc2R1JHWC1VdDBTQjBPNFpTeUhkTGViMEdXaFRHSW9OTXZAjQ2RxZAnluNVUZD';
+  const postTweet = async () => {
+    const tweetEndpoint = 'https://api.twitter.com/2/tweets';
+    const bearerToken = 'Y1724274340009566208-lamRqcWzM2BFGT2FHrB47cNCD6pYBm'; // Substitua pelo seu token de acesso Bearer
 
-		try {
-			const response = await axios.post(
-				`https://graph.facebook.com/v18.0/17841400008460056/media
-				?image_url=https://media.discordapp.net/attachments/441990298394951680/1171675902724296725/post.png?ex=655d8b37&is=654b1637&hm=3bb5b4e17c61519cd568f45c1ce003d71c5e3451a51495fd30d738f84bb73746&=&width=671&height=671
-				&caption=${caption}}`
-			);
+    try {
+      const response = await fetch(tweetEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${bearerToken}`,
+        },
+        body: JSON.stringify({
+          status: tweetText,
+        }),
+      });
 
-			console.log('Post successful:', response.data);
-		} catch (error) {
-			console.error('Error posting to Instagram:', error);
-		}
-	};
+      if (response.ok) {
+        console.log('Tweet postado com sucesso!');
+      } else {
+        console.error('Erro ao postar o tweet:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Erro na solicitação:', error);
+    }
+  };
 
-	React.useEffect(() => {
-		getInstafeed();
-	}, []);
+  return (
+    <div>
+      <textarea
+        placeholder="Digite seu tweet..."
+        value={tweetText}
+        onChange={handleTweetChange}
+      />
+      <button onClick={postTweet}>Postar Tweet</button>
+    </div>
+  );
+};
 
-	return (
-		<>
-			<Link
-				href={`https://api.instagram.com/oauth/authorize
-        ?client_id=347678657643128
-        &redirect_uri=https://posthigh.com.br/manage
-        &scope=user_profile,user_media
-        &response_type=code`}
-			>
-				<div className="border border-red-500 w-20">login instagram</div>
-			</Link>
+export default TwitterPost;
 
-			<div>Usuario logado: {user}</div>
-			<button onClick={handlePost}>Post to Instagram</button>
-
-			<button className="border border-red-500" onClick={() => signIn('instagram')}>
-				Sign in
-			</button>
-		</>
-	);
-}
