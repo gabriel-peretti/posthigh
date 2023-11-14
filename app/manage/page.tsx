8aeee8e7f69cb0843ce346811e0cd7a066c3ff1a
gabriel-peretti <gabriel.ferreira1@universo.univates.br>
2023-11-14 01:31:00 -0300

    fix twitter

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -7,51 +7,44 @@
 import React, { useState, ChangeEvent } from 'react';
 
 const TwitterPost = () => {
-  const [tweetText, setTweetText] = useState<string>(''); // Adicionei o tipo string aqui
+	const [tweetText, setTweetText] = useState<string>(''); // Adicionei o tipo string aqui
 
-  const handleTweetChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setTweetText(e.target.value);
-  };
+	const handleTweetChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		setTweetText(e.target.value);
+	};
 
-  const postTweet = async () => {
-    const tweetEndpoint = 'https://api.twitter.com/2/tweets';
-    const bearerToken = '1724274340009566208-lamRqcWzM2BFGT2FHrB47cNCD6pYBm';
+	const postTweet = async () => {
+		const tweetEndpoint = 'https://api.twitter.com/2/tweets';
+		const bearerToken = '1724274340009566208-lamRqcWzM2BFGT2FHrB47cNCD6pYBm';
 
-    try {
-      const response = await fetch(tweetEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${bearerToken}`,
-        },
-        body: JSON.stringify({
-          status: tweetText,
-        }),
-      });
+		try {
+			const response = await fetch(tweetEndpoint, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${bearerToken}`,
+				},
+				body: JSON.stringify({
+					status: tweetText,
+				}),
+			});
 
-      if (response.ok) {
-        console.log('Tweet postado com sucesso!');
-      } else {
-        console.error('Erro ao postar o tweet:', response.statusText);
-      }
-    } catch (error) {
-      console.error('Erro na solicitação:', error);
-    }
-  };
+			if (response.ok) {
+				console.log('Tweet postado com sucesso!');
+			} else {
+				console.error('Erro ao postar o tweet:', response.statusText);
+			}
+		} catch (error) {
+			console.error('Erro na solicitação:', error);
+		}
+	};
 
-  return (
-    <div>
-      <textarea
-        placeholder="Digite seu tweet..."
-        value={tweetText}
-        onChange={handleTweetChange}
-      />
-      <button onClick={postTweet}>Postar Tweet</button>
-    </div>
-  );
+	return (
+		<div>
+			<textarea placeholder="Digite seu tweet..." value={tweetText} onChange={handleTweetChange} />
+			<button onClick={postTweet}>Postar Tweet</button>
+		</div>
+	);
 };
 
 export default TwitterPost;
-
-
-
