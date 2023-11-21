3c266f5a9736ba9b52be90c1b774ccfdeb90ede4
gabriel-peretti <gabriel.ferreira1@universo.univates.br>
2023-11-21 00:38:50 -0300

    aplicacao final alterada

--- app/api/twitter/route.ts
+++ app/api/twitter/route.ts
@@ -1,15 +1,29 @@
+import { NextApiRequest } from 'next';
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
-import TwitterClient from '../../lib/twitter_client';
-import { NextApiRequest } from 'next';
+import { Client, auth } from 'twitter-api-sdk';
 
 export async function POST(request: Request) {
 	const body = await request.json();
 	const { tweetText } = body;
 	const token = await getToken({ req: request as unknown as NextApiRequest });
+	const { accessToken, refreshToken } = token || {};
 
-	const client = new TwitterClient(token);
-	client.createTweet(tweetText);
+	const authClient = new auth.OAuth2User({
+		client_id: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID as string,
+		client_secret: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET as string,
+		callback: 'http://localhost:3000/callback',
+		scopes: ['tweet.read', 'users.read', 'tweet.write', 'offline.access'],
+		token: {
+			access_token: accessToken as string,
+			refresh_token: refreshToken as string,
+			token_type: 'Bearer',
+		},
+	});
+
+	const client = new Client(authClient);
+
+	await client.tweets.createTweet({ text: tweetText });
 
 	return NextResponse.json({ status: 'ok' });
 }
