da6684962b7f06b523ab9a8a8780b62dbb0460c3
gabriel-peretti <gabriel.ferreira1@universo.univates.br>
2023-11-21 00:11:45 -0300

    alterando url de callback

--- app/lib/twitter_client.js
+++ app/lib/twitter_client.js
@@ -9,7 +9,8 @@ class TwitterAuthClientFactory {
 			const authClient = new auth.OAuth2User({
 				client_id: process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID,
 				client_secret: process.env.NEXT_PUBLIC_TWITTER_CLIENT_SECRET,
-				callback: 'http://localhost:3000/callback',
+				// callback: 'http://localhost:3000/callback',
+				callback: 'https://www.posthigh.com.br/api/auth/callback/twitter',
 				scopes: ['tweet.read', 'users.read', 'tweet.write', 'offline.access'],
 				token: {
 					access_token: token.accessToken,
